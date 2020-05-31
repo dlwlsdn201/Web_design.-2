153c2e7b3928a2c5b9a94361668b84776bab7b4c
dlwlsdn201 <53039583+dlwlsdn201@users.noreply.github.com>
2020-05-31 23:40:05 +0900

    Tried to preload
    -tried to preload images
    
    result : faild
    
    next To Do: Study more about preload logic

--- js/bg.js
+++ js/bg.js
@@ -5,21 +5,54 @@ const IMG_NUMBER = 4;
 
 function handleImgLoad() {
     console.log("finished loading.");
-    console.log("Hi");
+
 }
+
 
 
 function paintImage(imgNumber) {
     const image = new Image();
     image.src = `./bg/${imgNumber+1}.jpg`;
+    console.log(image.src);
     image.classList.add("bgImage");
     body.prepend(image);
     image.onreadystatechange = function () {
         if (image.readyState === "complete") {
             handleImgLoad();
         };
+        if (document.image) {
+            this.img = new Image();
+        }
     }
 }
+
+// function paintImage(imgNumber, PreImgArray) {
+//     const image = new Image();
+//     image.src = PreImgArray[`${imgNumber}`].src;
+//     image.classList.add("bgImage");
+//     body.prepend(image);
+//     image.onreadystatechange = function () {
+//         if (image.readyState === "complete") {
+//             handleImgLoad();
+//         };
+//     }
+// }
+
+
+// function imagepreload(ary) {
+//     try {
+//         if (!ary) return;
+//         arv = ary;
+//         var arr = new Array();
+//         for (var i = 0; i < arv.length; i++) {
+//             arr[i] = new Image();
+//             arr[i].src = arv[i];
+//         }
+
+//     } catch (e) {}
+//     return arr;
+// }
+
 
 function RandomBG() {
     const number = Math.floor(Math.random() * IMG_NUMBER);
@@ -29,8 +62,8 @@ function RandomBG() {
 
 function init() {
     const randomnumber = RandomBG();
-    paintImage(randomnumber);
     handleImgLoad();
+    paintImage(randomnumber);
+    // preImg = imagepreload(["./bg/1.jpg", "./bg/2.jpg", "./bg/3.jpg", "./bg/4.jpg"]);
+    // paintImage(randomnumber, preImg);
 }
-
-init();
