0bc780edf0caa06c534f38b6e25d45e4c0ed3c6f
dlwlsdn201 <53039583+dlwlsdn201@users.noreply.github.com>
2020-07-28 01:12:10 +0900

    -added scroll function
    -added scroll function in comments box

--- js/comment.js
+++ js/comment.js
@@ -2,66 +2,63 @@
 var boardArray = new Array();
 
 // 사용자 정의 객체 구성 (→ 생성자 함수 정의)
-function Board(uContent)
-{
+function Board(uContent) {
     // 주요 속성 구성
-    this.content=uContent;
-    this.writeDay=new Date();
+    this.content = uContent;
+    this.writeDay = new Date();
 }
 
 // 프로토타입 구성을 통해... 사용자 정의 객체의 함수 정의
 //프로토타입 구성을 통해 ... 사용자 정의 객체의 함수 정의
 //★★★★★★★★모든 객체는 prototype이라는 숨겨진 객체가 존재한다 알아두기 !!★★★★★★★★
-Board.prototype.userLocalString = function()
-{
-    return this.writeDay.getFullYear() + "-"
-        + (this.writeDay.getMonth()+1) + "-"
-        + this.writeDay.getDate() + "-"
-        + this.writeDay.getHours() + ":"
-        + this.writeDay.getSeconds();
-        
-        // 시 → getHours()
-        // 분 → getMinutes()
-        // 초 → getSeconds()          
+Board.prototype.userLocalString = function () {
+    return this.writeDay.getFullYear() + "-" +
+        (this.writeDay.getMonth() + 1) + "-" +
+        this.writeDay.getDate() + "-" +
+        this.writeDay.getHours() + ":" +
+        this.writeDay.getSeconds();
+
+    // 시 → getHours()
+    // 분 → getMinutes()
+    // 초 → getSeconds()          
 }
 
-function main()
-{
+function main() {
     //alert("함수 호출 확인");
-    
+
     var uContent = document.getElementById("uContent").value;
     var len = boardArray.length;
     //alert(len);      //-- 이 시점에서 테스트 시 → 0
-    
-    boardArray[len] = new Board(uContent);
-    //-- 『Board()』 → 사용자 정의 객체(생성자 함수)
-    
+
+
     //alert(len);// -- 작성이 이루어질 때 마다... 『+1』
-    
-    print(len);
-    
-    clear(); //폼 클리어 하도록 호출 
+    if (uContent.length === 0) {
+        alert('내용을 입력해주세요!');
+    } else {
+        boardArray[len] = new Board(uContent);
+        //-- 『Board()』 → 사용자 정의 객체(생성자 함수)
+        print(len);
+        clear(); //폼 클리어 하도록 호출 
+    }
 }
 
 // 내용 출력 함수
-function print(idx)
-{
+function print(idx) {
     //var strTemp = boardArray[idx].content;
     //alert(strTemp);
-    
+
     var tableNode = document.getElementById("bbsTable");
     var trNode = document.createElement("tr");
     trNode.className = "user_comment";
-    
-    trNode.appendChild(createTdNode((idx+1).toString()));
+
+    trNode.appendChild(createTdNode((idx + 1).toString()));
     trNode.appendChild(createTdNode(boardArray[idx].content));
     trNode.style.color = UserColor();
 
     tableNode.appendChild(trNode);
 }
 
-function createTdNode(val)
-{
+function createTdNode(val) {
     var textNode = document.createTextNode(val);
     var tdNode = document.createElement("td");
     tdNode.appendChild(textNode);
@@ -69,19 +66,18 @@ function createTdNode(val)
 }
 
 
-function clear()
-{
-    document.getElementById("uContent").value="";
-    
+function clear() {
+    document.getElementById("uContent").value = "";
+
 }
 
-function enterkey(){
-    if (window.event.keyCode == 13){
-       main();
+function enterkey() {
+    if (window.event.keyCode == 13) {
+        main();
     }
 }
 
-function UserColor(){
+function UserColor() {
     var fontColor = USER_COLOR;
     return fontColor;
 }
--- js/fontcolor.js
+++ js/fontcolor.js
@@ -1,40 +1,37 @@
-
-
 var USER_COLOR = '';
 
-function textColor(color){
-    var choice_color = ''    
+function textColor(color) {
+    var choice_color = ''
     maintain(color);
-    if (color === 'white'){
+    if (color === 'white') {
         choice_color = 'white';
-    }else if (color ==='yellow'){
+    } else if (color === 'yellow') {
         choice_color = 'yellow';
-    }else{
+    } else {
         choice_color = 'orange';
     }
 
-    
     USER_COLOR = choice_color;
 };
 
-function maintain(color){
-    var obj = document.querySelector('.'+color);
-    obj.addEventListener('click',function(){
-        if(color === 'white'){
+function maintain(color) {
+    var obj = document.querySelector('.' + color);
+    obj.addEventListener('click', function () {
+        if (color === 'white') {
             var other1 = document.querySelector('.yellow');
             var other2 = document.querySelector('.orange');
             other1.style.background = '';
             other1.style.color = '';
             other2.style.background = '';
             other2.style.color = '';
-        }else if(color ==='yellow'){
+        } else if (color === 'yellow') {
             var other1 = document.querySelector('.white');
             var other2 = document.querySelector('.orange');
             other1.style.background = '';
             other1.style.color = '';
             other2.style.background = '';
             other2.style.color = '';
-        }else{
+        } else {
             var other1 = document.querySelector('.white');
             var other2 = document.querySelector('.yellow');
             other1.style.background = '';
@@ -46,4 +43,3 @@ function maintain(color){
         obj.style.color = 'black';
     });
 }
-
